aced03a9023582d36a6547ee6a6d2f302d80acab
Amruta Kawade <65217380+AmrutaKawade@users.noreply.github.com>
2021-08-31 17:52:30 +0530

    Installation From GHAE (#15)
    * added validations for ghae url input
    
    * Installation from ghae
    
    * Fixing linting issues and testcases

--- static/js/ghae-register.js
+++ static/js/ghae-register.js
@@ -1,21 +1,12 @@
 $('.ghae-register-link').click((event) => {
   event.preventDefault();
   let ghaeUrl = document.getElementById('ghae_url_id').value;
-  if(ghaeUrl == ''){
-    document.getElementById("errormessage").innerHTML = `please enter a valid url.`;
-    return false;
-  }
-  if(!validateGhaeUrl(ghaeUrl)){
-    document.getElementById("errormessage").innerHTML = `"${ghaeUrl}" is not a valid GitHuB AE account url, please enter a valid url.`;
-    return false;
-  }
 
-  let ghaeInstanceHost = new URL(ghaeUrl).hostname;
   $.ajax({
     type: 'POST',
-    url: `/register?ghaeHost=${encodeURIComponent(ghaeInstanceHost)}`,
+    url: `/register?ghaeHost=${encodeURIComponent(ghaeUrl)}`,
     success(data) {
-      let ghaeInstance = `https://${ghaeInstanceHost}/settings/apps/new?state=${data.state}`;
+      let ghaeInstance = `https://${data.githubHost}/settings/apps/new?state=${data.state}`;
       document.getElementById('ghae_form_id').action = ghaeInstance;
       document.getElementById('manifest').value = data.manifest;
       document.getElementById('ghae_form_id').submit();
@@ -26,10 +17,3 @@ $('.ghae-register-link').click((event) => {
     }
   });
 });
-
-function validateGhaeUrl(url) {
-  if(url.indexOf("http://") == 0 || url.indexOf("https://") == 0){
-      return true;
-  }
-  return false; 
-}
